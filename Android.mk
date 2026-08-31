1dda5f2bb4f564df47ec3c569365b6f08c8bfab7
Scheldest-Audit <db-audit@github.com>
2026-08-31 20:43:42 +0000

    Shader Loaded Beta Test V0.1

--- Android.mk
+++ Android.mk
@@ -3,8 +3,8 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 LOCAL_CPP_EXTENSION := .cpp .cc
 LOCAL_MODULE    := amltemplate
-LOCAL_SRC_FILES := main.cpp mod/logger.cpp mod/config.cpp
+LOCAL_SRC_FILES := main.cpp ShaderLoader.cpp mod/logger.cpp mod/config.cpp
 LOCAL_CFLAGS += -O2 -mfloat-abi=softfp -DNDEBUG -std=c++17
 LOCAL_C_INCLUDES += ./include
-LOCAL_LDLIBS += -llog
+LOCAL_LDLIBS += -llog -lGLESv2 -lEGL
 include $(BUILD_SHARED_LIBRARY)
--- main.cpp
+++ main.cpp
@@ -2,34 +2,12 @@
 #include <mod/logger.h>
 #include <mod/config.h>
 
-MYMODCFG(net.dexsocy.mymod.guid, AML Mod Template, 1.0, Dexsociety)
+MYMODCFG(net.aqua.shaderloader, Shader Loader, 1.0, AquaNyot)
 
-uintptr_t pGameLibrary = 0;
-ConfigEntry* pCfgMyBestEntry;
+extern void ShaderLoader_Init();
 
 extern "C" void OnModLoad()
 {
-    logger->SetTag("Mod Template");
-
-    pGameLibrary = aml->GetLib("libGTASA.so");
-    if(pGameLibrary)
-    {
-        logger->Info("MyGame mod is loaded!");
-    }
-    else
-    {
-        logger->Error("MyGame mod is not loaded :(");
-        return; // Do not load our mod?
-    }
-
-    pCfgMyBestEntry = cfg->Bind("mySetting", "DefaultValue is 0?", "MyUniqueSection");
-    pCfgMyBestEntry->SetString("DefaultValue is unchanged");
-    pCfgMyBestEntry->SetInt(1);
-    pCfgMyBestEntry->Reset();
-    delete pCfgMyBestEntry; // Clean-up memory
-
-    bool bEnabled = cfg->Bind("Enable", true)->GetBool();
-    delete Config::pLastEntry; // Clean-up of the latest ConfigEntry*
-
-    cfg->Save(); // Will only save if something was changed
+    logger->SetTag("ShaderLoader");
+    ShaderLoader_Init();
 }
